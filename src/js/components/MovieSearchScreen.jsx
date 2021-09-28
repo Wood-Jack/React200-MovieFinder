f2a7b6452347f8622cf20d4fbf00debb3f57217c
Wood-Jack <thewoodrowz@gmail.com>
2021-09-28 11:16:51 -0400

    adding in parcel v2 to fix errors

---
+++ src/js/components/MovieSearchScreen.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default class MovieSearchScreen extends React.Component { 
+    
+    render() {
+   
+      return (
+
+        <div className='card' id='city-card-border'>
+            <div className= 'card-header' id= 'city-card-header'>Title</div>
+            <div className= 'card-body'>
+             
+
+            </div>
+        </div>
+      );
+    }
+}
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+
+
+
 import App from './app';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
+const store = createStore( 
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-/* eslint-enable */
 
+/* eslint-enable */
 
 render(
   <Provider store={ store }>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
